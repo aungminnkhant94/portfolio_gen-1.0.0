839ead064971e03f7ad35782c62cc2579831e72c
aungminnkhant94 <aungminnkhant94@gmail.com>
2022-04-12 15:57:32 +0630

    fontawesome

---
+++ src/components/ProfilePage.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1>Hello</h1>
+    <p>Pargraph</p> 
+    <font-awesome-icon icon="phone" />
+    <font-awesome-icon :icon="['fab', 'github']" />
+</template>
--- src/main.js
+++ src/main.js
@@ -5,4 +5,19 @@ import store from './store'
 
 import './assets/styles/main.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+// import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library */
+library.add(faPhone, faGithub)
+// library.add(faGithub)
+
+createApp(App).use(store).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
